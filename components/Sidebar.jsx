1eb684e43aa048c9be677fc1218dd6124be16518
alexandr khvostov <alexandr.khvostov@sysgears.com>
2021-06-15 11:10:45 +0300

    build(project): added yarn, changed imports in slug.jsx, updated package.json

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 
 function ListItems({ routes }) {
   if (routes) {
