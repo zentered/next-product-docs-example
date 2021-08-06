077935959338cd32a373ea61c72e0fd763ccb76d
Patrick Heneise <patrick@zentered.co>
2021-08-06 15:13:11 +0300

    chore: package and readme updates

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import { CheckIcon } from '@heroicons/react/outline'
 
 const features = [
   {
@@ -35,10 +34,6 @@ export default function Homepage() {
           {features.map((feature) => (
             <div key={feature.name} className="relative">
               <dt>
-                <CheckIcon
-                  className="absolute h-6 w-6 text-green-500"
-                  aria-hidden="true"
-                />
                 <p className="ml-9 text-lg leading-6 font-medium text-gray-900">
                   {feature.name}
                 </p>
